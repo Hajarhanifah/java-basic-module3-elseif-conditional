bff685809648d3f3fff9481a3a457f2582001e27
hajarhanifah <hajarhanifah@localhost>
2022-10-09 23:19:20 +0700

    add class Module3Conditional

---
+++ src/module3/conditional/CekKelulusan.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package module3.conditional;
+
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author hajarhanifah
+ */
+public class CekKelulusan {
+
+    public static void main(String[] args) {
+        String hasil = "";
+        String dataNilai = JOptionPane.showInputDialog("Masukkan nilai anda = ");
+        float nilai = Float.parseFloat(dataNilai);
+        if(nilai > 70){
+            hasil = "Anda Lulus!!";
+        } else {
+            hasil = "Anda Tidak Lulus";
+        }
+        
+        System.out.println(hasil);
+    }
+
+}
---
+++ src/module3/conditional/Module3Conditional.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package module3.conditional;
+
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author hajarhanifah
+ */
+public class Module3Conditional {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        String dataTekananDarah = JOptionPane
+                .showInputDialog("Masukkan tekanan darah sistolik Anda (mmHg)");
+        int tekananDarah = Integer.parseInt(dataTekananDarah);
+        if(tekananDarah > 140){
+            System.out.println("Anda Hipertensi");
+        }
+    }
+
+}
